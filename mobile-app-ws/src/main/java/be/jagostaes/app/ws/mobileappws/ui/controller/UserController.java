e8336429f933f4251eadfc4fca78e4b0fd80d55b
Jago Staes <jago.staes@gmail.com>
2019-06-11 10:02:19 +0200

    added xml dataformat response

--- mobile-app-ws/src/main/java/be/jagostaes/app/ws/mobileappws/ui/controller/UserController.java
+++ mobile-app-ws/src/main/java/be/jagostaes/app/ws/mobileappws/ui/controller/UserController.java
@@ -1,7 +1,10 @@
 package be.jagostaes.app.ws.mobileappws.ui.controller;
 
 import be.jagostaes.app.ws.mobileappws.ui.model.response.UserRest;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
+
+import java.awt.*;
 
 @RestController
 @RequestMapping("users") // http://localhost:8080/users
@@ -14,7 +17,11 @@ public class UserController {
         return "get user was called with page = " + page + " and limit = " + limit + " and sort = " + sort;
     }
 
-    @GetMapping(path = "/{userId}")
+    @GetMapping(path = "/{userId}",
+            produces = {
+                    MediaType.APPLICATION_XML_VALUE,
+                    MediaType.APPLICATION_JSON_VALUE
+                    } )
     public UserRest getUser(@PathVariable String userId){
         UserRest returnValue = new UserRest();
         returnValue.setFirstname("Jago");
