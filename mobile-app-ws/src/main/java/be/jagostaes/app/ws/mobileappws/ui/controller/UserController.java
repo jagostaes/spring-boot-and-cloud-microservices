584b14ee4f11b6d6cb153fc1fdb5954892e1e479
Jago Staes <jago.staes@gmail.com>
2019-06-11 14:28:41 +0200

    added HTTP POST request body validation using javax validation constraints

--- mobile-app-ws/src/main/java/be/jagostaes/app/ws/mobileappws/ui/controller/UserController.java
+++ mobile-app-ws/src/main/java/be/jagostaes/app/ws/mobileappws/ui/controller/UserController.java
@@ -7,6 +7,8 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+
 @RestController
 @RequestMapping("users") // http://localhost:8080/users
 public class UserController {
@@ -41,7 +43,7 @@ public class UserController {
                     MediaType.APPLICATION_XML_VALUE,
                     MediaType.APPLICATION_JSON_VALUE
                     } )
-    public ResponseEntity<UserRest> createUser(@RequestBody UserDetailsRequestModel userDetails){
+    public ResponseEntity<UserRest> createUser(@Valid @RequestBody UserDetailsRequestModel userDetails){
         UserRest returnValue = new UserRest();
         returnValue.setFirstname(userDetails.getFirstName());
         returnValue.setLastname(userDetails.getLastName());
--- mobile-app-ws/src/main/java/be/jagostaes/app/ws/mobileappws/ui/model/request/UserDetailsRequestModel.java
+++ mobile-app-ws/src/main/java/be/jagostaes/app/ws/mobileappws/ui/model/request/UserDetailsRequestModel.java
@@ -1,9 +1,22 @@
 package be.jagostaes.app.ws.mobileappws.ui.model.request;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 public class UserDetailsRequestModel {
+
+    @NotNull(message = "First name cannot be null")
+    @Size(min = 2, message = "First name must be at least 2 characters")
     private String firstName;
+    @NotNull(message = "Last name cannot be null")
+    @Size(min = 2, message = "Last name must be at least 2 characters")
     private String lastName;
+    @NotNull(message = "Email cannot be null")
+    @Email
     private String email;
+    @NotNull(message = "Password cannot be null")
+    @Size(min = 8, max = 16, message = "Password must be between 8 and 16 characters")
     private String password;
 
     public String getFirstName() {
