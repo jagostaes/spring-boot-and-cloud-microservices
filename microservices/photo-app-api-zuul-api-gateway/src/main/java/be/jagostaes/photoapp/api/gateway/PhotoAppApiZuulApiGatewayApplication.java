1e1a26806359c2b4cd5efd71562d542493ad76a5
Jago Staes <jago.staes@gmail.com>
2019-06-12 13:38:44 +0200

    simple implementation to allow access to users and user management through Zuul API gateway

--- microservices/photo-app-api-zuul-api-gateway/src/main/java/be/jagostaes/photoapp/api/gateway/PhotoAppApiZuulApiGatewayApplication.java
+++ microservices/photo-app-api-zuul-api-gateway/src/main/java/be/jagostaes/photoapp/api/gateway/PhotoAppApiZuulApiGatewayApplication.java
@@ -6,6 +6,8 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableZuulProxy
 public class PhotoAppApiZuulApiGatewayApplication {
 
     public static void main(String[] args) {
