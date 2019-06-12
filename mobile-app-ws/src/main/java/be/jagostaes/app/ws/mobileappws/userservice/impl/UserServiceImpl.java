88cb981b07f4f1ed35d1a5a8d88864559d2ab2c6
Jago Staes <jago.staes@gmail.com>
2019-06-12 09:47:30 +0200

    Added constructor based dependency injection to UserService

--- mobile-app-ws/src/main/java/be/jagostaes/app/ws/mobileappws/userservice/impl/UserServiceImpl.java
+++ mobile-app-ws/src/main/java/be/jagostaes/app/ws/mobileappws/userservice/impl/UserServiceImpl.java
@@ -1,18 +1,28 @@
 package be.jagostaes.app.ws.mobileappws.userservice.impl;
 
+import be.jagostaes.app.ws.mobileappws.shared.Utils;
 import be.jagostaes.app.ws.mobileappws.ui.model.request.UserDetailsRequestModel;
 import be.jagostaes.app.ws.mobileappws.ui.model.response.UserRest;
 import be.jagostaes.app.ws.mobileappws.userservice.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.UUID;
 
 @Service
 public class UserServiceImpl implements UserService {
 
     Map<String, UserRest> users;
+    Utils utils;
+
+    public UserServiceImpl() {
+    }
+
+    @Autowired
+    public UserServiceImpl(Utils utils) {
+        this.utils = utils;
+    }
 
     @Override
     public UserRest createUser(UserDetailsRequestModel userDetails) {
@@ -21,7 +31,7 @@ public class UserServiceImpl implements UserService {
         returnValue.setLastname(userDetails.getLastName());
         returnValue.setEmail(userDetails.getEmail());
 
-        String userId = UUID.randomUUID().toString();
+        String userId = utils.generateUserId();
         returnValue.setUserId(userId);
 
         if (users == null) users = new HashMap<>();
