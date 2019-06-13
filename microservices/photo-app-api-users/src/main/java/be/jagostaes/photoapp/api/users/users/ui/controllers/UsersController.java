a5e77701997412b2f02f3933a5c6dcc8725505e1
Jago Staes <jago.staes@gmail.com>
2019-06-13 11:57:50 +0200

    added h2 database dependency and configuration + create user request model class for http post calls

--- microservices/photo-app-api-users/src/main/java/be/jagostaes/photoapp/api/users/users/ui/controllers/UsersController.java
+++ microservices/photo-app-api-users/src/main/java/be/jagostaes/photoapp/api/users/users/ui/controllers/UsersController.java
@@ -1,10 +1,9 @@
 package be.jagostaes.photoapp.api.users.users.ui.controllers;
 
+import be.jagostaes.photoapp.api.users.users.ui.model.CreateUserRequestModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/users")
@@ -16,5 +15,10 @@ public class UsersController {
     @GetMapping("/status/check")
     public String status(){
         return "Working on port " + env.getProperty("local.server.port");
+    }
+
+    @PostMapping
+    public String createUser(@RequestBody CreateUserRequestModel userDetails){
+        return "Create user method is called";
     }
 }
