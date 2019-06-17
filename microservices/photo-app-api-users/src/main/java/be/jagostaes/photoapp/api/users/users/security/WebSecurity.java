d94b7df6f08b77b3f67887c6f51acf121d25b948
Jago Staes <jago.staes@gmail.com>
2019-06-17 16:02:35 +0200

    added support for returning xml or json to users microservice + added jackson-xml dependency and removed duplicate JPA dependency

--- microservices/photo-app-api-users/src/main/java/be/jagostaes/photoapp/api/users/users/security/WebSecurity.java
+++ microservices/photo-app-api-users/src/main/java/be/jagostaes/photoapp/api/users/users/security/WebSecurity.java
@@ -21,7 +21,7 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();      // disable cross site request forgery
-        http.authorizeRequests().antMatchers("/users/**").hasIpAddress(environment.getProperty("gateway.ip"));
+        http.authorizeRequests().antMatchers("/**").hasIpAddress(environment.getProperty("gateway.ip"));
         http.headers().frameOptions().disable();        //for h2 console
     }
 }
