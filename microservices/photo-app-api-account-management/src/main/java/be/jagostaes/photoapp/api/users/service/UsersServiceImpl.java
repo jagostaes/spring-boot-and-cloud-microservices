40fc2209f0b92d513f79eaf266ba5c5bebef88f9
Jago Staes <jago.staes@gmail.com>
2019-06-17 09:34:10 +0200

    Added extra attributes to userDto and generate uuid in UserServiceImpl

--- microservices/photo-app-api-account-management/src/main/java/be/jagostaes/photoapp/api/users/service/UsersServiceImpl.java
+++ microservices/photo-app-api-account-management/src/main/java/be/jagostaes/photoapp/api/users/service/UsersServiceImpl.java
@@ -2,10 +2,16 @@ package be.jagostaes.photoapp.api.users.service;
 
 import be.jagostaes.photoapp.api.users.shared.UserDto;
 
+import java.util.UUID;
+
 public class UsersServiceImpl implements UsersService{
 
     @Override
     public UserDto createUser(UserDto userDetails) {
+        userDetails.setUserId(UUID.randomUUID().toString());
+
         return null;
     }
+
+
 }
