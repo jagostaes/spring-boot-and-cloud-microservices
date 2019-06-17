11d6c4ea5f08fa3b02c1a64a283559c91be2ecf8
Jago Staes <jago.staes@gmail.com>
2019-06-17 10:32:55 +0200

    Added JPA dependency and generated random userId

--- microservices/photo-app-api-users/src/main/java/be/jagostaes/photoapp/api/users/users/service/UsersServiceImpl.java
+++ microservices/photo-app-api-users/src/main/java/be/jagostaes/photoapp/api/users/users/service/UsersServiceImpl.java
@@ -2,9 +2,13 @@ package be.jagostaes.photoapp.api.users.users.service;
 
 import be.jagostaes.photoapp.api.users.users.shared.UserDto;
 
+import java.util.UUID;
+
 public class UsersServiceImpl implements UsersService {
     @Override
     public UserDto createUser(UserDto userDetails) {
+        userDetails.setUserId(UUID.randomUUID().toString());
+
         return null;
     }
 }
